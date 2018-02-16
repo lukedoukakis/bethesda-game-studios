cba26762c327bc14f2944d1d61e21cad9ecdd06d
lukedoukakis <35055929+lukedoukakis@users.noreply.github.com>
2018-02-15 21:47:21 -0800

    Add files via upload

---
+++ War.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class War extends CardGame{
+	
+	Scanner s = new Scanner(System.in);
+	Random r = new Random();
+	
+	public void deal(){
+		for(int i = 0; i < 26; i++){
+			for(int j = 0; j < players.size(); j++){
+				card = deck.get(r.nextInt(deck.size()));
+				deck.remove(card);
+				players.get(i).hand.add(card);
+			}
+		}
+	}
+	
+	public void runGame(){
+		
+		numOfPlayers = 2;
+		addPlayers(numOfPlayers);
+		assembleDeck();
+		deal();
+		
+		int user; //user's input for this class
+		
+		System.out.println("Welcome to War. This is a two player game.");
+		//todo: take players through the game
+		
+	
+		
+		
+	}
+}

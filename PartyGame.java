bd7ab4eb0ffea20e9656a906f4108b218eb61d09
lukedoukakis <35055929+lukedoukakis@users.noreply.github.com>
2018-02-15 21:53:03 -0800

    Update PartyGame.java

--- PartyGame.java
+++ PartyGame.java
@@ -4,9 +4,9 @@ public abstract class PartyGame {
 	
 	protected int numOfPlayers;
 	
-	ArrayList<Player> players;
+	ArrayList<Player> players; //games will cycle through this list for players' turns?
 	
-	void addPlayers(int amount){
+	void addPlayers(int amount){ //adds players to players ArrayList
 		for(int i = 0; i < amount; i++){
 			players.add(new Player());
 		}

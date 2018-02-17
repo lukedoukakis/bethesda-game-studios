e589d7e0bd6465f17379f47564264a2b2723a062
lukedoukakis <35055929+lukedoukakis@users.noreply.github.com>
2018-02-16 23:48:20 -0800

    Update War.java

--- War.java
+++ War.java
@@ -3,6 +3,9 @@ import java.util.Random;
 
 public class War extends CardGame{
 	
+	int p1Score;
+	int p2Score;
+	
 	Scanner s = new Scanner(System.in);
 	Random r = new Random();
 	
@@ -23,10 +26,35 @@ public class War extends CardGame{
 		assembleDeck();
 		deal();
 		
-		int user; //user's input for this class
+		String user; //user's input for this class
 		
-		System.out.println("Welcome to War. This is a two player game.");
-		//todo: take players through game
+		System.out.println("Welcome to War. This is a two player game where the deck will be split up between each player. Each round, the players will play the card on at the top of their hand, and the player with the higher value card will take both cards. The player with the most cards at the end wins!");
+		
+		System.out.println("Player 1, what is your name?");
+		players.get(0).setName(s.nextLine());
+		
+		System.out.println("Player 2, what is your name?");
+		players.get(1).setName(s.nextLine());
+		
+		for(int i = 0; i < 26; i++){
+			if(Integer.parseInt(players.get(0).hand.get(i).value) > Integer.parseInt(players.get(1).hand.get(i).value)){
+				p1Score+=2;
+				System.out.println(players.get(0).getName() + " wins the round. His/her card, a "+ players.get(0).hand.get(i).toString()+ " beat "+ players.get(1).getName() +"'s "+ players.get(1).hand.get(i).toString() + ".");
+			}
+			else{
+				p2Score+=2;
+				System.out.println(players.get(1).getName() + " wins the round. His/her card, a "+ players.get(1).hand.get(i).toString()+ " beat "+ players.get(0).getName() +"'s "+ players.get(0).hand.get(i).toString() + ".");
+			}
+			System.out.println("Press any key to move to the next round.");
+			user = s.nextLine();
+			
+		}
+		System.out.println("Game over!");
+		if(p1Score > p2Score){
+			System.out.println(players.get(0).getName() + " wins with a score of " + p1Score + ".");
+		}else{
+			System.out.println(players.get(1).getName() + " wins with a score of " + p2Score + ".");
+		}
 		
 	
 		
